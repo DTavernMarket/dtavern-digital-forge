091ab0ecc6e750453aa8a3e5210f9be23789ab5c
Barvous <marcostkd20147@gmail.com>
2026-03-31 21:16:26 -0300

    insere credenciais do firebase em hml nas variaveis de ambiente

--- src/environment/environment.hml.ts
+++ src/environment/environment.hml.ts
@@ -5,4 +5,12 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'https://dtavern-server-hml.up.railway.app/api/v1',
+  firebaseConfig: {
+    apiKey: 'AIzaSyCgF4TDJNsB76P079D3VBjzw3898JVz9U4',
+    authDomain: 'dtavern-hml.firebaseapp.com',
+    projectId: 'dtavern-hml',
+    storageBucket: 'dtavern-hml.firebasestorage.app',
+    messagingSenderId: '830865137830',
+    appId: '1:830865137830:web:bcef5034adc7172d385467'
+  }
 } as const;
--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -8,4 +8,13 @@ export const environment = {
    * URL base da API backend (inclui `/api/v1`).
    */
   apiBaseUrl: 'http://localhost:8080/api/v1',
+  firebaseConfig: {
+    apiKey: 'AIzaSyB-zeSKQuVmP8JEHq3Qu9PJBLbaNwHGY6U',
+    authDomain: 'dtavern-marketplace.firebaseapp.com',
+    projectId: 'dtavern-marketplace',
+    storageBucket: 'dtavern-marketplace.firebasestorage.app',
+    messagingSenderId: '1019613186354',
+    appId: '1:1019613186354:web:17bc23876bb37583036f2b',
+    measurementId: 'G-ERE85K7G1E'
+  }
 } as const;
